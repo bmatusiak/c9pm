7d760475262f1908cd95fbab6866a8f27b962cbb
Jan Jongboom <janjongboom@gmail.com>
2012-10-17 11:08:29 +0300

    Update vmc to delete the gem symlink

--- packages/vmc-0.0.1/install.sh
+++ packages/vmc-0.0.1/install.sh
@@ -22,3 +22,7 @@ bin/vmc target api.cloudfoundry.com
 
 # add to PATH cause symlinking no work...
 echo export PATH=\$PATH:$DEST/bin >> $HOME/etc/.bashrc
+
+# move the gem installed vmc to some other place
+# because it only interfers with our nice real vmc
+rm ~/lib/ruby/gems/bin/vmc
